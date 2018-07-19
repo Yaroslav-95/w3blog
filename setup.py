adb87583cc63ca228a6492dc641f3e1389edc629
Yaroslav de la Peña <contact@yaroslavps.com>
2018-07-19 22:23:15 +0300

    html classes for better customization options

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='w3blog',
-    version='0.4-testing.13',
+    version='0.4-testing.14',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
