d42e64a09e6e1898d4423d786bc48a47a8b8fc0e
Yaroslav de la Peña Smirnov <contact@yaroslavps.com>
2018-10-08 00:03:23 +0300

    dynamic (no infinite) scrolling working on categories, index (not archive)

--- weblog/static/weblog/js/weblog.js
+++ weblog/static/weblog/js/weblog.js
@@ -12,5 +12,23 @@ function toggleNode(caller){
     target.classList.toggle('show');
 }
 
-function loadBlogPosts(page = 2, category = null){
+function loadBlogPosts(url, page = 2){
+    var req = new XMLHttpRequest();
+    function insert(){
+        if (this.status == 200 && this.readyState == 4){
+            var blog_content = document.querySelector(".blog-content");
+            try{
+                var nxtpage_button = document.querySelector(".nxtpage-button");
+                nxtpage_button.insertAdjacentHTML("beforebegin", this.responseText);
+            }
+            catch(er){
+                console.log("error "+er);
+                blog_content.insertAdjacentHTML("beforeend", this.responseText);
+            }
+            console.log(this.responseText);
+        }
+    }
+    req.addEventListener("readystatechange", insert);
+    req.open("GET", url, true);
+    req.send();
 }
